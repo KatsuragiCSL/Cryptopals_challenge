eced4ca8fcf8e80027cc50eb30b7d969eaa4e56e
KatsuragiCSL <shue12345678@gmail.com>
2019-04-14 23:21:38 -0400

    complete challenge 14

--- set2/14.py
+++ set2/14.py
@@ -10,11 +10,16 @@ aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
 dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
 YnkK'''
 
+#Skipping blocksize detection here for simplicity
+
 key = urandom(16)
+#length of random prefix. The case is similar when length > 15 since we will just need to deal with one more dummy block of prefix
+l = randint(1,15)
+random_prefix = urandom(l)
 
 def AES_128_ECB(plain, key):
     #concat
-    plain += base64.b64decode(unknown)
+    plain = random_prefix + plain + base64.b64decode(unknown)
     plain = padding(plain, 16)
     cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
     encryptor = cipher.encryptor()
@@ -27,59 +32,46 @@ def padding(block, length):
     else:
         return (block + bytes(chr(n)*n, 'utf-8'))
 
-def checkRepeatedBlock(cipher, keysize = 16):
-    if len(cipher) % keysize != 0:
-        n = len(cipher) // keysize + 1
-    else:
-        n = len(cipher) // keysize
-
-    blocks = [cipher[i*keysize:(i+1)*keysize] for i in range(n)]
-
-    if len(set(blocks)) != n:    #ECB
-        return 1
-    else:    #CBC
-        return 0
-
-def detectblocksize():
-    #feed A's to oracle until blocks doesn't change
-    for i in range(1,32):
+def detect_prefix_length():
+    #feed A's to oracle until the first doesn't change
+    for i in range(1,16):
         s1 = bytes("A"*i, 'utf-8')
         s2 = bytes("A"*(i+1), 'utf-8')
         msg_pre = AES_128_ECB(s1, key)
         msg_cur = AES_128_ECB(s2, key)
-        if msg_pre[:i] == msg_cur[:i]:
-            return i
+        if msg_pre[:16] == msg_cur[:16]:
+            return (16 - i)
 
-def getUnknownLength(keysize):
-    #length of 'unknown' is not simply length of encrypting empty strength! That would give a number larger than the actual length becuz of Padding is attached!
-
-    compare1 = len(AES_128_ECB(bytes("", 'utf-8'), key))
-    for i in range(1, keysize):
-        compare2 = len(AES_128_ECB(bytes("T"*i, 'utf-8'), key))
-        #when filled up a full block and open a new block
-        if compare2 != compare1:
-            return (compare1 - i - 1)
-    return compare1
-
-def guessUnknown(known, keysize):
+def guessUnknown(known, keysize, prefix_size):
     k = len(known)
+    f = prefix_size
     #number of A's to leave one byte to guess
-    p = keysize - (k % keysize) -1
+    p = keysize - ((k+f) % keysize) -1
     plain = bytes("A"*p, 'utf-8')
-    block = k // keysize
+    block = (k+f) // keysize
     for i in range(256):
         test = AES_128_ECB(plain + known + bytes([i]), key)[block*keysize:(block+1)*keysize]
         if AES_128_ECB(plain, key)[block*keysize:(block+1)*keysize] == test:
             #bytes(5) returns five \x00. It faked me. LOL
             return bytes([i])
 
+def smashPadding(b, block_length):
+    for i in range(block_length):
+        if i == 0:
+            pass
+        elif (b[-i:] == bytes(chr(i)*i, 'utf-8')):
+            return b[:-i]
+    return b
+
 if __name__ == '__main__':
-    keysize = detectblocksize()
-    isECB = checkRepeatedBlock(AES_128_ECB(bytes("A"*100, 'utf-8'), key))
-    l_unknown = getUnknownLength(keysize)
-    if isECB:
-        known = b''
-        for r in range(l_unknown):
-            new_known = guessUnknown(known, keysize)
+    known = b''
+    prefix_size = detect_prefix_length()
+    for r in range(200):
+        try:
+            new_known = guessUnknown(known, 16, prefix_size)
             known = known + new_known
-        print(known.decode())
+        except:
+            pass
+    known = smashPadding(known, 16)
+    print(known.decode())
+
