5007043aff4cd2a5e6474c33e5348b7b5dc6cdb0
KatsuragiCSL <shue12345678@gmail.com>
2019-04-29 23:17:11 -0400

    tried ch19, not weighting of alphabets leads to wrong results

---
+++ set3/19.py
@@ -0,0 +1,108 @@
+import cryptography
+from base64 import b64decode
+from os import urandom
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+backend = default_backend()
+
+secrets = '''SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==
+Q29taW5nIHdpdGggdml2aWQgZmFjZXM=
+RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==
+RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=
+SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk
+T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==
+T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=
+UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==
+QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=
+T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl
+VG8gcGxlYXNlIGEgY29tcGFuaW9u
+QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==
+QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=
+QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==
+QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=
+QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=
+VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==
+SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==
+SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==
+VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==
+V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==
+V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==
+U2hlIHJvZGUgdG8gaGFycmllcnM/
+VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=
+QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=
+VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=
+V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=
+SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==
+U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==
+U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=
+VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==
+QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu
+SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=
+VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs
+WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=
+SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0
+SW4gdGhlIGNhc3VhbCBjb21lZHk7
+SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=
+VHJhbnNmb3JtZWQgdXR0ZXJseTo=
+QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4='''
+
+secrets = secrets.split('\n')
+
+nonce = 0
+
+key = urandom(16)
+
+def XOR(x, y):
+    return bytes([a^b for (a,b) in zip(x, y)])
+
+def encryptAESblock(block, key):
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=backend)
+    encryptor = cipher.encryptor()
+    return encryptor.update(block) + encryptor.finalize()
+
+def key_stream_gen(key, nonce):
+    counter = 0
+    while True:
+        stream = nonce.to_bytes(8, byteorder="little") + counter.to_bytes(8, byteorder="little")
+        key_stream = encryptAESblock(stream, key)
+        yield from key_stream
+        counter += 1
+
+def AES_128_CTR_decrypt(ctxt, key, nonce):
+    key_stream = key_stream_gen(key, nonce)
+    if type(ctxt) != bytes:
+        ctxt = bytes(ctxt, 'utf-8')
+    return XOR(ctxt, key_stream)
+
+ctxts = [AES_128_CTR_decrypt(secret, key, nonce) for secret in secrets]
+asc = list(range(97, 122)) + list(range(65, 91)) + [32] + [33] + [39] + [44] + [46] +[63]
+asc = [bytes([x]) for x in asc]
+
+def guess_one_position(p):
+    results = {"score":0}
+    for key in range(256):
+        key = bytes([key])
+        plain = []
+        for ctxt in ctxts:
+            try:
+                plain.append(XOR(key, bytes([ctxt[p]])))
+            except:
+                pass
+        score = sum([x in asc for x in plain])
+        if score > results["score"]:
+            results = {"key_byte":key, "score":score}
+    return results["key_byte"]
+
+def guess_plaintexts():
+    key_stream = b''
+    for i in range(max([len(s) for s in secrets])):
+        key_stream += guess_one_position(i)
+    return key_stream
+
+if __name__ == '__main__':
+    key_stream = guess_plaintexts()
+    print(key_stream)
+    plains = [XOR(key_stream, ctxt) for ctxt in ctxts]
+    for plain in plains:
+        print(plain)
